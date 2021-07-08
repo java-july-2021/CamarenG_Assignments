b04fed49f0cb20ca7e56072c8e3fc443cfd68f77
Cam <camarengaineslinuxsandbox@gmail.com>
2021-07-07 18:40:25 -0600

    First part Done

---
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -0,0 +1,8 @@
+
+
+public class StringManipulator{
+    public String trimAndConcat(String a, String b){
+        String str = a.trim() + b.trim();
+        return str;
+    }
+}
---
+++ 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
@@ -0,0 +1,11 @@
+
+
+public class StringManipulatorTesting {
+    public static void main(String[] args){
+        StringManipulator manipulator = new StringManipulator();
+String str = manipulator.trimAndConcat("    Hello     ","     World    ");
+System.out.println(str); // HelloWorld 
+
+
+    }
+}
