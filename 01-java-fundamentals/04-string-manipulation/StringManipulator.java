591459d0cbe2c9a3159a2af710351a47e46f50bf
Cam <camarengaineslinuxsandbox@gmail.com>
2021-07-07 19:49:55 -0600

    All parts done, would like to ask about the values on .substring and why 1, 2 only show 1 char not 2 - fixed formatting

--- 01-java-fundamentals/04-string-manipulation/StringManipulator.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -1,28 +1,27 @@
 
-
-public class StringManipulator{
-    public String trimAndConcat(String a1, String b1){
+public class StringManipulator {
+    public String trimAndConcat(String a1, String b1) {
         String str = a1.trim() + b1.trim();
         return str;
     }
-    public Integer getIndexOrNull(String d, char e){
-        if(d.indexOf(e) <= 0){
+
+    public Integer getIndexOrNull(String d, char e) {
+        if (d.indexOf(e) <= 0) {
             return null;
-        }
-        else{
-        return d.indexOf(e);
+        } else {
+            return d.indexOf(e);
         }
     }
-    public Integer getIndexOrNull(String d1, String d2){
-        if(d1.indexOf(d2) <= 0){
+
+    public Integer getIndexOrNull(String d1, String d2) {
+        if (d1.indexOf(d2) <= 0) {
             return null;
-        }
-        else{
+        } else {
             return d1.indexOf(d2);
         }
     }
 
-    public String concatSubstring(String a3, int x, int y, String b3){
-    return a3.substring(x, y) + b3;
+    public String concatSubstring(String a3, int x, int y, String b3) {
+        return a3.substring(x, y) + b3;
     }
 }
