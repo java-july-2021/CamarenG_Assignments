0c5a848a0de0df891909b8008b73782081f3ae2a
Cam <camarengaineslinuxsandbox@gmail.com>
2021-07-07 18:57:47 -0600

    Second Part Done

--- 01-java-fundamentals/04-string-manipulation/StringManipulator.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -1,8 +1,16 @@
 
 
 public class StringManipulator{
-    public String trimAndConcat(String a, String b){
-        String str = a.trim() + b.trim();
+    public String trimAndConcat(String a1, String b1){
+        String str = a1.trim() + b1.trim();
         return str;
+    }
+    public Integer getIndexOrNull(String d, char e){
+        if(d.indexOf(e) <= 0){
+            return null;
+        }
+        else{
+        return d.indexOf(e);
+        }
     }
 }
--- 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
@@ -7,5 +7,16 @@ String str = manipulator.trimAndConcat("    Hello     ","     World    ");
 System.out.println(str); // HelloWorld 
 
 
+//---------------------------------------------------------------
+char letter = 'o';
+Integer a = manipulator.getIndexOrNull("Coding", letter);
+Integer b = manipulator.getIndexOrNull("Hello World", letter);
+Integer c = manipulator.getIndexOrNull("Hi", letter);
+System.out.println(a); // 1
+System.out.println(b); // 4
+System.out.println(c); // null
+
+
+
     }
 }
