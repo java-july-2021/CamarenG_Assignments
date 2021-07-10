9ce63bb9a1b8745f8942c4521dd9478be8530869
Cam <camarengaineslinuxsandbox@gmail.com>
2021-07-09 19:30:13 -0600

    Done

--- 01-java-fundamentals/07-map-of-the-hashmatique/hashmaps.java
+++ 01-java-fundamentals/07-map-of-the-hashmatique/hashmaps.java
@@ -1,20 +1,20 @@
 import java.util.HashMap;
 
 public class hashmaps {
-    public void music(){
-        HashMap<String, String> trackList = new HashMap<String, String>(); 
+    public void music() {
+        HashMap<String, String> trackList = new HashMap<String, String>();
 
         trackList.put("Its Friday!", "Its friday friday, its finally friday!");
-        trackList.put("last friday night (TGIF)", "Last Friday night Yeah, we danced on tabletops And we took too many shots");
+        trackList.put("last friday night (TGIF)",
+                "Last Friday night Yeah, we danced on tabletops And we took too many shots");
         trackList.put("SelfCare", "I switched the time zone, but what do I know?");
         trackList.put("Blinding Lights", "I've been tryin' to call ");
         trackList.remove("Its Friday!");
-        for(HashMap.Entry<String, String> entry : trackList.entrySet()){
+        for (HashMap.Entry<String, String> entry : trackList.entrySet()) {
 
-            System.out.println("Track: " + entry.getKey()+ " Lyrics: " + entry.getValue());
+            System.out.println("Track: " + entry.getKey() + " Lyrics: " + entry.getValue());
 
         }
-
 
     }
 }
--- 01-java-fundamentals/07-map-of-the-hashmatique/hashmapsTest.java
+++ 01-java-fundamentals/07-map-of-the-hashmatique/hashmapsTest.java
@@ -5,6 +5,5 @@ public class hashmapsTest {
 
         hash.music();
 
-
     }
 }
